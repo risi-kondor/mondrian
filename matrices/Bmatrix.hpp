1c79468e102148dd89d2379c2f41f36e192b7002
Risi Kondor <risi@uchicago.edu>
2017-01-04 00:56:52 -0600

    'changes'

--- matrices/Bmatrix.hpp
+++ matrices/Bmatrix.hpp
@@ -1,3 +1,31 @@
+/* ---------------------------------------------------------------------------
+ 
+  Mondrian: open source multithreaded matrix library 
+
+  Copyright (C) 2017 Imre Risi Kondor
+  Copyright (C) 2015 Imre Risi Kondor, Nedelina Teneva, Pramod K Mudrakarta
+
+  Parts of the following code are derived from the pMMF library 
+  (https://github.com/risi-kondor/pMMF) which is licensed under the 
+  GNU Public License, version 3. This code therefore is also licensed 
+  under the terms of the GNU Public License, version 3. 
+ 
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 3
+  of the License, or (at your option) any later version.
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+ --------------------------------------------------------------------------- */
+
+
 #ifndef _Bmatrix
 #define _Bmatrix
 
